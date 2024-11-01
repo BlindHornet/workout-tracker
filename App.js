cf8f20e5fe76dcba3423c72770b70e1b98e16eb7
BlindHornet <36351996+BlindHornet@users.noreply.github.com>
2024-11-01 11:08:23 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import exercises from "./assets/data/exercises.json";
 
 export default function App() {
+  const exercise = exercises[0];
+
+  function capitalize(str) {
+    return str && String(str[0]).toUpperCase() + String(str).slice(1);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <Text style={styles.exerciseSubtitle}>
+          {capitalize(exercise.muscle)} | {capitalize(exercise.equipment)}
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "gainsboro",
+    justifyContent: "center",
+    padding: 10,
+  },
+  exerciseContainer: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: 500,
+  },
+  exerciseSubtitle: {
+    color: "dimgray",
   },
 });
